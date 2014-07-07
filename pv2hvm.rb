690100f77e8b2cc08c29a36734875f9ac445660e
j3tm0t0 <j3tm0t0@gmail.com>
2014-07-07 19:05:13 +0900

    added --cleanup mode, added some error messages

--- pv2hvm.rb
+++ pv2hvm.rb
@@ -18,8 +18,8 @@ class Pv2hvm
   end
 
   def check_ami(ami)
-    abort("virtualization_type must be paravirtual") if ami.virtualization_type != :paravirtual
-    abort("root_device_type must be EBS") if ami.root_device_type != :ebs
+    abort("ERROR: virtualization_type must be paravirtual") if ami.virtualization_type != :paravirtual
+    abort("ERROR: root_device_type must be EBS") if ami.root_device_type != :ebs
   end
 
   def convert
@@ -37,22 +37,39 @@ class Pv2hvm
       dst_vol_size=@src[:volume_size]
     end
 
-    puts "creating target volume with size : "+dst_vol_size.to_s
-    @dst_vol = @ec2.volumes.create(:size => dst_vol_size , :availability_zone => @az, :volume_type => "gp2")
-    sleep 1 while @dst_vol.status != :available
-    @dst_attachment = @dst_vol.attach_to(@ec2.instances[@instanceId], "/dev/sdo")
-    sleep 1 while @dst_attachment.status != :attached
-    sleep 1 while !File.blockdev?("/dev/sdo")
-    puts " #{@dst_vol.id} created and attached to /dev/sdo"
-
     puts "creating source volume from snapshot : "+@src[:snapshot_id]
-    @src_vol = @ec2.volumes.create(:snapshot_id => @src[:snapshot_id], :availability_zone => @az, :volume_type => "gp2")
+    begin
+      @src_vol = @ec2.volumes.create(:snapshot_id => @src[:snapshot_id], :availability_zone => @az, :volume_type => "gp2")
+    rescue
+      abort <<EOS
+ERROR: Create volume from snapshot failed. This could be caused by either of followings.
+1) snapshot is deleted.
+2) AMI is not yours and AMI owner does not allow you to create volume.
+3) instance profile has insufficient permission.
+4) volume size or volume count limit exceeded.
+EOS
+    end
     sleep 1 while @src_vol.status != :available
     @src_attachment = @src_vol.attach_to(@ec2.instances[@instanceId], "/dev/sdm")
     sleep 1 while @src_attachment.status != :attached
     sleep 1 while !File.blockdev?("/dev/sdm")
     puts " #{@src_vol.id} created and attached to /dev/sdm"
 
+    puts "creating target volume with size : "+dst_vol_size.to_s
+    begin
+      @dst_vol = @ec2.volumes.create(:size => dst_vol_size , :availability_zone => @az, :volume_type => "gp2")
+    rescue
+      abort <<EOS
+ERROR: Create volume failed. This could be caused by either of followings.
+1) instance profile has insufficient permission.
+2) volume size or volume count limit exceeded.
+EOS
+    end
+    sleep 1 while @dst_vol.status != :available
+    @dst_attachment = @dst_vol.attach_to(@ec2.instances[@instanceId], "/dev/sdo")
+    sleep 1 while @dst_attachment.status != :attached
+    sleep 1 while !File.blockdev?("/dev/sdo")
+    puts " #{@dst_vol.id} created and attached to /dev/sdo"
     puts ""
 
   end
@@ -116,30 +133,56 @@ class Pv2hvm
 
   def cleanup
     puts "-- cleanup"
-    self.delete_volumes()
-  end
-
-  def delete_volumes
-    puts "deleting volumes"
-    [@src_vol,@dst_vol].each{ |vol|
-      vol.attachments.each{ |attachment|
-        attachment.delete(:force => true)
-      }
-      sleep 1 until vol.status == :available
-      vol.delete
-      puts " #{vol.id} deleted"
-    }
+    self.class.delete_volumes([@src_vol,@dst_vol])
   end
 
   def ec2metadata(path)
-    Net::HTTP.get '169.254.169.254', '/latest/meta-data/'+path
+    self.class.ec2metadata(path)
+  end
+
+  class <<self
+    def ec2metadata(path)
+      Net::HTTP.get '169.254.169.254', '/latest/meta-data/'+path
+    end
+
+    def cleanup
+      puts "-- cleanup"
+      system('umount -f /mnt &> /dev/null')
+      sleep 1 while system("df | grep /mnt &> /dev/null")
+      @instanceId=ec2metadata("instance-id")
+      @az=ec2metadata("placement/availability-zone")
+      @region=@az.sub(/.$/,'')
+      AWS.config(:region => @region)
+      @ec2 = AWS::EC2.new()
+      vols=@ec2.instances[@instanceId].attachments.select{|dev| dev.match(/sd[mo]/)}.map{|d,a| a.volume}
+      self.delete_volumes(vols)
+    end
+
+    def delete_volumes(vols)
+      puts "deleting volumes"
+      vols.each{ |vol|
+        vol.attachments.each{ |attachment|
+          attachment.delete(:force => true)
+        }
+        sleep 1 until vol.status == :available
+        vol.delete
+        puts " #{vol.id} deleted"
+      }
+    end
+
   end
 
 end
 
+
 if (ARGV.length==0)
-  puts "usage: pv2hvm.rb ami-12345678 [ ami-01234567 ...]"
-else
+  puts <<EOS
+usage: pv2hvm.rb ami-12345678 [ ami-01234567 ...]
+       pv2hvm.rb --cleanup # unmount and detach and delete volumes
+EOS
+elsif ARGV[0]=="--cleanup"
+  Pv2hvm.cleanup
+elsif
   ARGV.each{|ami|
     pv2hvm=Pv2hvm.new(ami)
     pv2hvm.convert
